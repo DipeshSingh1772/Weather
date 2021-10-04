0d2afb630190c2da82662712fd8a1ae549aa9c03
DipeshSingh1772 <dipeshsingh1772@gmail.com>
2021-10-04 17:56:20 +0530

    Tablayout added

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.weather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tabLayout: TabLayout
+    lateinit var tabItem1: TabItem
+    lateinit var tabItem2: TabItem
+    lateinit var tabItem3: TabItem
+    lateinit var viewPager: ViewPager2
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tabLayout)
+        tabItem1 = findViewById(R.id.tabItem1)
+        tabItem2 = findViewById(R.id.tabItem2)
+        tabItem3 = findViewById(R.id.tabItem3)
+        viewPager= findViewById(R.id.viewPager)
+
     }
 }
--- app/src/main/java/com/example/weather/TendaysFragment.kt
+++ app/src/main/java/com/example/weather/TendaysFragment.kt
@@ -5,21 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weather.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
 
-    private var _binding : FragmentMainBinding? = null
-    private val binding get() = _binding!!
+class TendaysFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        _binding = FragmentMainBinding.inflate(inflater,container,false)
-
-        return binding.root
+        return inflater.inflate(R.layout.fragment_tendays, container, false)
     }
+
+
 }
